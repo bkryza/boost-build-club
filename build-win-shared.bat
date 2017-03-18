2725891b9641dd01d258c2f2f115d903f0a0f114
Bartosz Kryza <bkryza@gmail.com>
2017-03-18 21:44:11 +0100

    Updated to VS2017

--- build-win-shared.bat
+++ build-win-shared.bat
@@ -3,9 +3,9 @@ call bootstrap.bat
 copy bbc-config-win-shared.jam project-config.jam
 
 echo
-echo To build boost DLLs for VS2015, x86:
-echo b2 --toolset=msvc-14.0 --build-type=complete stage link=shared
+echo To build boost DLLs for VS2017, x86:
+echo b2 --toolset=msvc-15.0 --build-type=complete stage link=shared
 echo
-echo Building boost DLLs for VS2015, x64
+echo Building boost DLLs for VS2017, x64
 echo
-b2 -j 16 --toolset=msvc-14.0 --build-type=complete architecture=x86 address-model=64 stage link=shared
+b2 -j 8 --toolset=msvc-15.0 --build-type=complete architecture=x86 address-model=64 stage link=shared
--- build-win-static.bat
+++ build-win-static.bat
@@ -5,8 +5,8 @@ copy boost-build-club-project-config.jam project-config.jam
 echo
 echo Building boost static libs for VS2010, x86
 echo
-echo b2 --toolset=msvc-14.0 --build-type=complete stage link=static
+echo b2 --toolset=msvc-15.0 --build-type=complete stage link=static
 echo
 echo Building boost static libs for VS2010, x64
 echo
-b2 -j 16 --toolset=msvc-14.0 --build-type=complete architecture=x86 address-model=64 stage link=static
+b2 -j 8 --toolset=msvc-15.0 --build-type=complete architecture=x86 address-model=64 stage link=static
